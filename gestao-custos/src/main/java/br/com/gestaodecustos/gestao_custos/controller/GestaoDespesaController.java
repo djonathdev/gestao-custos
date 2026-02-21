5c7fa20d5da3f582ef834775a8f6829bd2db0a32
diegodjo <diegodjonath30@gmail.com>
2026-02-21 18:31:37 -0300

    Adicionado o Banco

--- gestao-custos/src/main/java/br/com/gestaodecustos/gestao_custos/controller/GestaoDespesaController.java
+++ gestao-custos/src/main/java/br/com/gestaodecustos/gestao_custos/controller/GestaoDespesaController.java
@@ -1,9 +1,15 @@
 package br.com.gestaodecustos.gestao_custos.controller;
 
+import br.com.gestaodecustos.gestao_custos.entity.Despesa;
+import br.com.gestaodecustos.gestao_custos.useCases.CadastroDespesaUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/gestao")
+@RestController
 public class GestaoDespesaController {
 
     /*
@@ -12,8 +18,12 @@ public class GestaoDespesaController {
     * - Criar entidade
     */
 
+    @Autowired
+    CadastroDespesaUseCase cadastroDespesaUseCase;
     @PostMapping("/create")
-    public void create() {
+    public void create(@RequestBody Despesa despesa) {
+
+        cadastroDespesaUseCase.execute(despesa);
 
     }
 
--- gestao-custos/src/main/java/br/com/gestaodecustos/gestao_custos/entity/Despesa.java
+++ gestao-custos/src/main/java/br/com/gestaodecustos/gestao_custos/entity/Despesa.java
@@ -1,13 +1,97 @@
 package br.com.gestaodecustos.gestao_custos.entity;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
+
+@Entity
+@Table(name ="despesa")
 public class Despesa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
     private String descricao;
     private LocalDate data;
     private BigDecimal valor;
-    private String categoria;
 
+    @Column(length = 100)
+    private String categoria;
+    private String email;
+
+    @CreatedDate()
+    private LocalDate data_criacao;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getData_criacao() {
+        return data_criacao;
+    }
+
+    public void setData_criacao(LocalDate data_criacao) {
+        this.data_criacao = data_criacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Despesa{" +
+                "id=" + id +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                ", valor=" + valor +
+                ", categoria='" + categoria + '\'' +
+                ", email='" + email + '\'' +
+                ", data_criacao=" + data_criacao +
+                '}';
+    }
 }
